4f2a0390dbc1904b45930ee48d92c7a43185313b
kiran963783 <161558692+kiran963783@users.noreply.github.com>
2024-03-08 16:39:08 +0530

    Add files via upload

---
+++ countMonthlyMembers.java
@@ -0,0 +1,26 @@
+package com.messmanagementsystem.manager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class countMonthlyMembers {
+
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/mess_db";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "1234";
+
+    public static void countMonthlyMembers() throws SQLException {
+        try (Connection conn = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+             PreparedStatement pstmt = conn.prepareStatement("SELECT COUNT(*) FROM candidates WHERE category = 'Monthly'")) {
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                int monthlyMembers = rs.getInt(1);
+                System.out.println("Total monthly members: " + monthlyMembers);
+            }
+        }
+    }
+
+}
